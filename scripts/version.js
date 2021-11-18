c52d0bc80ef61db1af8c5f6e0a700e030c0227e3
Chris <42108459+impossible98@users.noreply.github.com>
2021-11-19 01:49:37 +0800

    feat: init project

--- scripts/version.js
+++ scripts/version.js
@@ -4,6 +4,7 @@ const path = require("path");
 const VERSION_PATH = path.join(__filename, "../../VERSION");
 const PKG_PATH = path.join(__filename, "../../package.json");
 const pkg = require("../package.json");
+const GRREN = "\x1B[32m%s\x1B[0m";
 
 async function writeVersion() {
     const data = await fs.readFile(VERSION_PATH, "UTF-8");
@@ -14,8 +15,16 @@ async function writeVersion() {
     await fs.writeFile(PKG_PATH, newPkg);
 }
 
+function showVersion() {
+    const name = pkg.name;
+    const version = pkg.version;
+
+    console.log("\x1B[32m%s\x1B[0m", `${name} version: ${version}`);
+}
+
 async function main() {
     await writeVersion();
+    showVersion();
 }
 
 main();
