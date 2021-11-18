462b659de5376bb1bf3c888182a405b055ad869a
Chris <42108459+impossible98@users.noreply.github.com>
2021-11-19 00:29:02 +0800

    feat: init project

--- scripts/version.js
+++ scripts/version.js
@@ -1,24 +1,20 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 
 const VERSION_PATH = path.join(__filename, "../../VERSION");
 const PKG_PATH = path.join(__filename, "../../package.json");
 const pkg = require("../package.json");
 
-function main() {
-    fs.readFile(VERSION_PATH, "UTF-8", (err, data) => {
-        if (err) {
-            throw err;
-        }
+async function writeVersion() {
+    const data = await fs.readFile(VERSION_PATH, "UTF-8")
+    pkg.version = data.trim();
 
-        pkg.version = data.trim();
+    await fs.writeFile(PKG_PATH, JSON.stringify(pkg))
+}
 
-        fs.writeFile(PKG_PATH, JSON.stringify(pkg), err => {
-            if (err) {
-                throw err;
-            }
-        });
-    });
+
+async function main() {
+    await writeVersion();
 }
 
 main();
